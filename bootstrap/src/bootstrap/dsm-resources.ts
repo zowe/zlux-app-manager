7ed6b370138eaf1ac8a37c393821440ad0480a71
Jordan Filteau <jfilteau@rocketsoftware.com>
2018-09-06 20:07:55 -0400

    Finished implementing notification framework
    Signed-off-by: Jordan Filteau <jfilteau@rocketsoftware.com>

--- bootstrap/src/bootstrap/dsm-resources.ts
+++ bootstrap/src/bootstrap/dsm-resources.ts
@@ -4,17 +4,18 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 
-import { PluginManager } from 'zlux-base/plugin-manager/plugin-manager' 
+import { PluginManager } from 'zlux-base/plugin-manager/plugin-manager'
 import { DsmUri } from '../uri/dsm-uri'
-import { Dispatcher } from 'zlux-base/dispatcher/dispatcher' 
+import { Dispatcher } from 'zlux-base/dispatcher/dispatcher'
 import { Logger } from '../../../../zlux-shared/src/logging/logger'
-import { Registry } from 'zlux-base/registry/registry' 
+import { Registry } from 'zlux-base/registry/registry'
+import { NotificationManager } from 'zlux-base/notification-manager/notification-manager'
 // import { VirtualDesktopAdapter } from '../abstract-virtual-desktop/virtual-desktop-adapter'
 
 declare var window: { RocketMVD: typeof DSMResources };
@@ -27,15 +28,15 @@ export class DSMResources {
   static dispatcher:Dispatcher = new Dispatcher(logger);
   static logger:Logger = logger;
   static registry:ZLUX.Registry = new Registry()
+  static NotificationManager = new NotificationManager();
 }
 
 /*
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
-
--- bootstrap/src/bootstrap/rocket-mvd-resources.ts
+++ bootstrap/src/bootstrap/rocket-mvd-resources.ts
@@ -4,17 +4,18 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 
-import { PluginManager } from 'zlux-base/plugin-manager/plugin-manager' 
+import { PluginManager } from 'zlux-base/plugin-manager/plugin-manager'
 import { MvdUri } from '../uri/mvd-uri'
-import { Dispatcher } from 'zlux-base/dispatcher/dispatcher' 
+import { Dispatcher } from 'zlux-base/dispatcher/dispatcher'
 import { Logger } from '../../../../zlux-shared/src/logging/logger'
 import { Registry } from 'zlux-base/registry/registry'
+import { NotificationManager } from 'zlux-base/notification-manager/notification-manager'
 // import { VirtualDesktopAdapter } from '../abstract-virtual-desktop/virtual-desktop-adapter'
 
 declare var window: { RocketMVD: typeof RocketMVDResources,
@@ -30,6 +31,7 @@ export class RocketMVDResources {
   static dispatcher:Dispatcher = new Dispatcher(logger);
   static logger:Logger = logger;
   static registry:ZLUX.Registry = new Registry();
+  static NotificationManager = new NotificationManager();
 }
 
 
@@ -37,9 +39,8 @@ export class RocketMVDResources {
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
-
