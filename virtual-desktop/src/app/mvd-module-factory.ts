0d18421c4c181ca07571f231736460c9dfee56cd
Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>
2018-10-08 13:37:55 +0500

    Begin with i18n
    Signed-off-by: Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>
    Signed-off-by: Baoyuan Xing <bxing@rocketsoftware.com>

--- virtual-desktop/src/app/mvd-module-factory.ts
+++ virtual-desktop/src/app/mvd-module-factory.ts
@@ -13,15 +13,17 @@
 // Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule, Type } from '@angular/core';
+import { NgModule, Type, StaticProvider } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 
 // Modules
 import { PluginManagerModule } from './plugin-manager/plugin-manager.module';
 import { ApplicationManagerModule } from './application-manager/application-manager.module';
 import { AuthenticationModule } from './authentication-manager/authentication-manager.module';
 import { SharedModule } from './shared/shared.module';
+import { PluginManager } from 'zlux-base/plugin-manager/plugin-manager';
 
 export class MvdModuleFactory {
   static generateModule(windowManagerModule: Type<any>, mainComponent: Type<any>): Type<any> {
@@ -43,6 +45,37 @@ export class MvdModuleFactory {
       bootstrap: [mainComponent]
     })(class MvdModule {});
   }
+
+  public static getTranslationProviders(): Promise<StaticProvider[]> {
+    const language: string = 'ru'; // this.languageLocaleService.getLanguage();
+    const noProviders: StaticProvider[] = [];
+    if (!language || language === 'en') {
+      return Promise.resolve(noProviders);
+    }
+    const translationFile = MvdModuleFactory.getTranslationFileURL(language);
+    console.log(`translation file is ${translationFile}`);
+    return MvdModuleFactory.getTranslations(translationFile)
+      .then( (translations: string ) => [
+        { provide: TRANSLATIONS, useValue: translations },
+        { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+        { provide: LOCALE_ID, useValue: language },
+      ])
+      .catch(() => noProviders); // ignore if file not found
+  }
+
+  private static getTranslationFileURL(language: string): string {
+    const desktopPlugin = PluginManager.getDesktopPlugin();
+    console.log(`getTranslationFileURL`, PluginManager);
+    if (desktopPlugin) {
+      return ZoweZLUX.uriBroker.pluginResourceUri(desktopPlugin, `assets/i18n/messages.${language}.xlf`);
+    }
+    return 'err_no_desktop_plugin';
+  }
+
+  private static getTranslations(file: string): Promise<string> {
+    return window.fetch(file).then(response => response.text());
+  }
+
 }
 
 
