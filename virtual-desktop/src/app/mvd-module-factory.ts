0a6035ed4c69e0b36fc415c71133c36e916855a2
Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>
2018-10-12 12:11:41 +0500

    Add TranslationLoaderService
    Signed-off-by: Leonty Chudinov <34669449+lchudinov@users.noreply.github.com>

--- virtual-desktop/src/app/mvd-module-factory.ts
+++ virtual-desktop/src/app/mvd-module-factory.ts
@@ -14,18 +14,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, Type, StaticProvider } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpModule} from '@angular/http';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
 
 // Modules
 import { PluginManagerModule } from './plugin-manager/plugin-manager.module';
 import { ApplicationManagerModule } from './application-manager/application-manager.module';
 import { AuthenticationModule } from './authentication-manager/authentication-manager.module';
 import { SharedModule } from './shared/shared.module';
-import { PluginManager } from 'zlux-base/plugin-manager/plugin-manager';
+import { LanguageLocaleService } from './shared/language-locale.service';
+import { TranslationLoaderService } from './shared/translation-loader.service';
 
 export class MvdModuleFactory {
+  private static localeService: LanguageLocaleService;
+  private static translationLoaderService: TranslationLoaderService;
+
   static generateModule(windowManagerModule: Type<any>, mainComponent: Type<any>): Type<any> {
     return NgModule({
       imports: [
@@ -40,40 +43,32 @@ export class MvdModuleFactory {
         AuthenticationModule
       ],
       providers: [
-        { provide: LocationStrategy, useClass: HashLocationStrategy }
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        { provide: LanguageLocaleService, useFactory: MvdModuleFactory.getLocaleService},
+        { provide: TranslationLoaderService, useFactory: MvdModuleFactory.getTranslationLoaderService}
       ],
       bootstrap: [mainComponent]
     })(class MvdModule {});
   }
 
   public static getTranslationProviders(): Promise<StaticProvider[]> {
-    const language: string = 'ru'; // this.languageLocaleService.getLanguage();
-    const noProviders: StaticProvider[] = [];
-    if (!language || language === 'en') {
-      return Promise.resolve(noProviders);
-    }
-    const translationFile = MvdModuleFactory.getTranslationFileURL(language);
-    console.log(`translation file is ${translationFile}`);
-    return MvdModuleFactory.getTranslations(translationFile)
-      .then( (translations: string ) => [
-        { provide: TRANSLATIONS, useValue: translations },
-        { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
-        { provide: LOCALE_ID, useValue: language },
-      ])
-      .catch(() => noProviders); // ignore if file not found
+    const translationLoader = MvdModuleFactory.getTranslationLoaderService();
+    return translationLoader.getTranslationProviders(ZoweZLUX.pluginManager.getDesktopPlugin());
   }
 
-  private static getTranslationFileURL(language: string): string {
-    const desktopPlugin = PluginManager.getDesktopPlugin();
-    console.log(`getTranslationFileURL`, PluginManager);
-    if (desktopPlugin) {
-      return ZoweZLUX.uriBroker.pluginResourceUri(desktopPlugin, `assets/i18n/messages.${language}.xlf`);
+  private static getLocaleService(): LanguageLocaleService {
+    if (!MvdModuleFactory.localeService) {
+      MvdModuleFactory.localeService = new LanguageLocaleService();
     }
-    return 'err_no_desktop_plugin';
+    return MvdModuleFactory.localeService;
   }
 
-  private static getTranslations(file: string): Promise<string> {
-    return window.fetch(file).then(response => response.text());
+  private static getTranslationLoaderService(): TranslationLoaderService {
+    if (!MvdModuleFactory.translationLoaderService) {
+      const languageLocaleService = MvdModuleFactory.getLocaleService();
+      MvdModuleFactory.translationLoaderService = new TranslationLoaderService(languageLocaleService);
+    }
+    return MvdModuleFactory.translationLoaderService;
   }
 
 }
