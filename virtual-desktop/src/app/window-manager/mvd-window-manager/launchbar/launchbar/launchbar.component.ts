ff247b60e6ef23895a9378f2fc91fc9cad737193
Baoyuan Xing <bxing@rocketsoftware.com>
2018-10-30 12:00:12 -0400

    bug fix Signed-off-by: Baoyuan Xing <bxing@rocketsoftware.com>
    Signed-off-by: Suneeth Keerthy <skeerthy@rocketsoftware.com>

--- virtual-desktop/src/app/window-manager/mvd-window-manager/launchbar/launchbar/launchbar.component.ts
+++ virtual-desktop/src/app/window-manager/mvd-window-manager/launchbar/launchbar/launchbar.component.ts
@@ -176,7 +176,7 @@ export class LaunchbarComponent {
   launchPluginPropertyWindow(plugin: DesktopPluginDefinitionImpl){
   
     let propertyWindowID = this.windowManager.getWindow(this.propertyWindowPluginDef);
-    if (propertyWindowID){
+    if (propertyWindowID!=null){
       this.windowManager.showWindow(propertyWindowID);
     } else {
       this.applicationManager.spawnApplication(this.propertyWindowPluginDef,this.getAppPropertyInformation(plugin));
