3046722085f20f78c7737722a4af8dd17bdaa1b7
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-08-08 17:13:54 -0400

    Had a bug where screenshot only triggered when it was the same app as before
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- virtual-desktop/src/app/window-manager/mvd-window-manager/shared/window-manager.service.ts
+++ virtual-desktop/src/app/window-manager/mvd-window-manager/shared/window-manager.service.ts
@@ -54,7 +54,8 @@ export class WindowManagerService implements MVDWindowManagement.WindowManagerSe
 
   private focusedWindow: DesktopWindow | null;
   private topZIndex: number;
-  private _lastScreenshotId: number = -1;
+  private _lastScreenshotPluginId: string = '';  
+  private _lastScreenshotWindowId: number = -1;
   public showPersonalizationPanel: boolean = false;
   /*
    * NOTES:
@@ -261,7 +262,7 @@ export class WindowManagerService implements MVDWindowManagement.WindowManagerSe
       restore: () => this.restore(windowId),
       setTitle: (title) => this.setWindowTitle(windowId, title),
       setPosition: (pos) => this.setPosition(windowId, pos),
-      spawnContextMenu: (xRel, yRel, items) => this.spawnContextMenu(windowId, xRel, yRel, items),
+      spawnContextMenu: (xPos, yPos, items, isAbsolutePos?:boolean) => this.spawnContextMenu(windowId, xPos, yPos, items, isAbsolutePos),
       registerCloseHandler: (handler)=> this.registerCloseHandler(windowId, handler)
     };
   }
@@ -493,7 +494,7 @@ export class WindowManagerService implements MVDWindowManagement.WindowManagerSe
       return false;
     }
     let requestScreenshot = false;
-    if (!this.windowHasFocus(destination) && this._lastScreenshotId != destination){
+    if (!this.windowHasFocus(destination) && this._lastScreenshotWindowId != destination){
       requestScreenshot = true;
     }
 
@@ -506,8 +507,9 @@ export class WindowManagerService implements MVDWindowManagement.WindowManagerSe
     desktopWindow.windowState.zIndex = this.topZIndex ++;
     if (requestScreenshot){
       setTimeout(()=> {
-        this.screenshotRequestEmitter.next({pluginId: desktopWindow.plugin.getIdentifier(), windowId: this._lastScreenshotId});
-        this._lastScreenshotId = destination;
+        this.screenshotRequestEmitter.next({pluginId: this._lastScreenshotPluginId, windowId: this._lastScreenshotWindowId});
+        this._lastScreenshotWindowId = destination;
+        this._lastScreenshotPluginId = desktopWindow.plugin.getIdentifier();
       },500); //delay a bit for performance perception
     }
     return true;
@@ -657,19 +659,22 @@ export class WindowManagerService implements MVDWindowManagement.WindowManagerSe
         break;
     }
   }
-  
-  spawnContextMenu(windowId: MVDWindowManagement.WindowId, xRelative: number, yRelative: number, items: ContextMenuItem[]): void {
+
+  spawnContextMenu(windowId: MVDWindowManagement.WindowId, x: number, y: number, items: ContextMenuItem[], isAbsolutePos?: boolean): void {
     const desktopWindow = this.windowMap.get(windowId);
     if (desktopWindow == null) {
       throw new Error('Attempted to spawn context menu for null window');
     }
-
-    const newX = desktopWindow.windowState.position.left + xRelative;
-    const newY = desktopWindow.windowState.position.top + yRelative + WindowManagerService.WINDOW_HEADER_HEIGHT;
-
-    this.contextMenuRequested.next({xPos: newX, yPos: newY, items: items});
+    const windowPos = desktopWindow.windowState.position;
+    const newX = isAbsolutePos ? x : windowPos.left + x;
+    const newY = isAbsolutePos ? y : windowPos.top + y + WindowManagerService.WINDOW_HEADER_HEIGHT;
+    if ((newX >= windowPos.left && newX <= (windowPos.left+windowPos.width))
+         && (newY >= windowPos.top && newY <= (windowPos.top+windowPos.height))) {
+      this.contextMenuRequested.next({xPos: newX, yPos: newY, items: items});    
+    } else {
+      this.logger.warn(`Rejecting context menu due to invalid coord ${newX},${newY} for app at ${windowPos.left},${windowPos.top} w=${windowPos.width}, h=${windowPos.height}`);
+    }
   }
-
 }
 
 
