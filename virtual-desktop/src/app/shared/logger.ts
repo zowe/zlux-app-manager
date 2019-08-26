ed4b9c2687aacce1af4dc34eceae8e7cf90ceef4
Leanid Astrakou <lastrakou@rocketsoftware.com>
2019-08-26 14:55:57 -0400

    Added support of log messages for BaseLogger
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- virtual-desktop/src/app/shared/logger.ts
+++ virtual-desktop/src/app/shared/logger.ts
@@ -7,5 +7,45 @@
   
   Copyright Contributors to the Zowe Project.
 */
+//import { PluginManager } from 'app/plugin-manager/shared/plugin-manager';
+// import { L10nConfigService } from '../i18n/l10n-config.service';
+// import { HttpClient } from '@angular/common/http';
+// import { Component } from '@angular/core';
+//import { PluginManager } from '../plugin-manager/shared/plugin-manager';
 
-export const BaseLogger = ZoweZLUX.logger.makeComponentLogger('org.zowe.zlux.ng2desktop'); //the one hardcoded place
+// var pluginManager = ZoweZLUX.pluginManager;
+// var l10nConfigService: L10nConfigService;
+// var http: HttpClient;
+
+const baseLoggerIdentifier = 'org.zowe.zlux.ng2desktop' //the one hardcoded place
+export var BaseLogger: any = ZoweZLUX.logger.makeComponentLogger(baseLoggerIdentifier);
+
+let lang = ZoweZLUX.globalization.getLanguage();
+let plugin = ZoweZLUX.pluginManager.getDesktopPlugin();
+let messageLoc = ZoweZLUX.uriBroker.pluginResourceUri(plugin, `assets/i18n/log/messages_${lang}.json`);
+
+fetch(messageLoc) // Attempt to find log messages for global language
+.then((resp) => resp.json())
+.then(function(messages) {
+    BaseLogger._messages = messages;
+    afterBaseLoggerInit();
+})
+.catch(function() { // If it doesn't work...
+    lang = "en"; // Try English log messages (default)
+    messageLoc = ZoweZLUX.uriBroker.pluginResourceUri(plugin, `assets/i18n/log/messages_${lang}.json`);
+
+    fetch(messageLoc)
+    .then((resp) => resp.json())
+    .then(function(messages) {
+        BaseLogger._messages = messages;
+        afterBaseLoggerInit();
+    })
+    .catch(function() { // If that still doesn't work, just do nothing...
+      afterBaseLoggerInit();
+    });
+
+});
+
+function afterBaseLoggerInit() {
+  BaseLogger.info(`App framework initializing. User agent=${navigator.userAgent}`);
+}
