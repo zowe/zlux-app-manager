035dcc537c604cc021b98087437d4cb0f89b2dd5
1000TurquoisePogs <sgrady@rocketsoftware.com>
2020-03-13 10:55:56 -0400

    Un-hardcoded plugin id references
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- system-apps/app-generator/webClient/src/typings.d.ts
+++ system-apps/app-generator/webClient/src/typings.d.ts
@@ -13,6 +13,8 @@ declare var module: {
 	id: string;
 };
 
+declare var ZoweZLUX: any;
+
 /*
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
