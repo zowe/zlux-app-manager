e313e2f550e667a84bfbd4465c3c17fdd326ba70
Leonty Chudinov <lchudinov@rocketsoftware.com>
2020-03-20 16:30:24 +0500

    Web Browser App: Add missing copyrights
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- system-apps/web-browser-app/nodeServer/ts/csp.ts
+++ system-apps/web-browser-app/nodeServer/ts/csp.ts
@@ -1,3 +1,13 @@
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
+
 // Content Security Policy Parser
 
 export interface ContentSecurityPolicy {
@@ -58,3 +68,13 @@ export class CSP {
     return Object.keys(csp).map(key => `${key} ${csp[key]}`).join(';');
   }
 }
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Copyright Contributors to the Zowe Project.
+*/
