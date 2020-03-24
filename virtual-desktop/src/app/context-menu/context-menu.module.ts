a8659807371b95f8a7894c4885d51dd94e79cd21
1000TurquoisePogs <sgrady@rocketsoftware.com>
2020-03-24 16:10:47 -0400

    Fixing context menu for i18n
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- virtual-desktop/src/app/context-menu/context-menu.module.ts
+++ virtual-desktop/src/app/context-menu/context-menu.module.ts
@@ -4,49 +4,41 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-
+  
   SPDX-License-Identifier: EPL-2.0
-
+  
   Copyright Contributors to the Zowe Project.
 */
 
 import { NgModule } from '@angular/core';
+import { ContextMenuComponent } from './context-menu.component';
 import { CommonModule } from '@angular/common';
-
-import { SimpleWindowManagerService } from './simple-window-manager.service';
-import { SimpleComponent } from './simple.component';
-import { ApplicationManagerModule } from '../../application-manager/application-manager.module';
-import { AuthenticationModule } from '../../authentication-manager/authentication-manager.module';
-import { ContextMenuModule } from '../../context-menu/context-menu.module';
 
 @NgModule({
   imports: [
-    ApplicationManagerModule,
-    AuthenticationModule,
-    CommonModule,
-    ContextMenuModule
+    CommonModule
   ],
   declarations: [
-    SimpleComponent
+    ContextMenuComponent
   ],
   exports: [
-
+    ContextMenuComponent
   ],
   providers: [
-    { provide: MVDWindowManagement.Tokens.WindowManagerToken, useClass: SimpleWindowManagerService }
   ]
 })
-export class SimpleWindowManagerModule {
+export class ContextMenuModule {
 
 }
+
 
 /*
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-
+  
   SPDX-License-Identifier: EPL-2.0
-
+  
   Copyright Contributors to the Zowe Project.
 */
 
