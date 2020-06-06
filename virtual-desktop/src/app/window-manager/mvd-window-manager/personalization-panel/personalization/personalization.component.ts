dcd7bb26f196086e6e95adac9882fc6564f11d74
Leanid Astrakou <lastrakou@rocketsoftware.com>
2020-06-05 20:08:37 -0400

    Better UX to show currently selected color
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>

--- virtual-desktop/src/app/window-manager/mvd-window-manager/personalization-panel/personalization/personalization.component.ts
+++ virtual-desktop/src/app/window-manager/mvd-window-manager/personalization-panel/personalization/personalization.component.ts
@@ -86,9 +86,7 @@ export class PersonalizationComponent implements OnInit {
     if (selectedColorHSL != null) {
       this.updateLightnessSwatches(selectedColorHSL);
     }
-    if (this.selectedColor == Colors.COOLGREY_90) {
-      this.selectedCircle = 16;
-    }
+    this.whichColorAmI();
   }
 
   /* More info on conversion chart can be found at: https://css-tricks.com/converting-color-spaces-in-javascript/ */
@@ -367,6 +365,15 @@ export class PersonalizationComponent implements OnInit {
 
   goBack(): void {
     this.desktopThemeService.goBack();
+  }
+
+  whichColorAmI(): void {
+    var index: number;
+    for (index = 1; index <= this.paletteColors.length; index++) {
+      if (this.selectedColor == this.paletteColors[index]) {
+        this.selectedCircle = index;
+      }
+    }
   }
 
 }
