23b34282c7c8eab5c950167eaa7416c3a4c602e8
Leanid Astrakou <lastrakou@rocketsoftware.com>
2020-06-24 21:45:25 -0400

    Clean-up
    Signed-off-by: Leanid Astrakou <lastrakou@rocketsoftware.com>
    Signed-off-by: Mitesh <miteshgoplani@gmail.com>

--- virtual-desktop/src/app/window-manager/mvd-window-manager/personalization-panel/personalization/personalization.component.ts
+++ virtual-desktop/src/app/window-manager/mvd-window-manager/personalization-panel/personalization/personalization.component.ts
@@ -102,7 +102,9 @@ export class PersonalizationComponent implements OnInit {
     if (selectedColorHSL != null) {
       this.updateLightnessSwatches(selectedColorHSL);
     }
-    this.whichColorAmI();
+    if (this.selectedColor == Colors.COOLGREY_90) {
+      this.selectedCircle = 16;
+    }
   }
 
   /* More info on conversion chart can be found at: https://css-tricks.com/converting-color-spaces-in-javascript/ */
@@ -394,15 +396,6 @@ export class PersonalizationComponent implements OnInit {
 
   goBack(): void {
     this.desktopThemeService.goBack();
-  }
-
-  whichColorAmI(): void {
-    var index: number;
-    for (index = 1; index <= this.paletteColors.length; index++) {
-      if (this.selectedColor == this.paletteColors[index]) {
-        this.selectedCircle = index;
-      }
-    }
   }
 
 }
