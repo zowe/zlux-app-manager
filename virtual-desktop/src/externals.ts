ac6da5db01483e22b589df0f497c0c5b38f18026
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-11-10 09:04:16 +0500

    Remove rxjs-comat from externals.js
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- virtual-desktop/src/externals.ts
+++ virtual-desktop/src/externals.ts
@@ -36,7 +36,8 @@ const libs: { [index: string]: {library: any} } = {
   '@angular/router': require('@angular/router'),
   '@angular/animations': require('@angular/animations'),
   'angular-l10n': require('angular-l10n'),
-  'rxjs/Rx': require('rxjs/Rx')
+  'rxjs': require('rxjs'),
+  'rxjs/operators': require('rxjs/operators')
 };
 
 /* Expose modules to requirejs */
