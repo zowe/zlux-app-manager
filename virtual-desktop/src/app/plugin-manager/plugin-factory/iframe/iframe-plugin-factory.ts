854b90d6fa236c3d1974e1f5ee84c340329e14e0
Leonty Chudinov <lchudinov@rocketsoftware.com>
2021-11-23 12:42:20 +0500

    enable jit for generated modules
    Signed-off-by: Leonty Chudinov <lchudinov@rocketsoftware.com>

--- virtual-desktop/src/app/plugin-manager/plugin-factory/iframe/iframe-plugin-factory.ts
+++ virtual-desktop/src/app/plugin-manager/plugin-factory/iframe/iframe-plugin-factory.ts
@@ -4,9 +4,9 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 
@@ -120,7 +120,8 @@ export class IFramePluginFactory extends PluginFactory {
     @NgModule({
       imports: [CommonModule],
       declarations: [decoratedComponent],
-      entryComponents: [decoratedComponent]
+      entryComponents: [decoratedComponent],
+      jit: true
     })
     class RuntimePluginModule {}
     return this.compiler.compileModuleAsync(RuntimePluginModule).then(factory =>
@@ -154,9 +155,9 @@ window.addEventListener("mouseup", (event) => {
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 
--- virtual-desktop/src/app/plugin-manager/plugin-factory/react/react-plugin-factory.ts
+++ virtual-desktop/src/app/plugin-manager/plugin-factory/react/react-plugin-factory.ts
@@ -4,9 +4,9 @@
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 
@@ -50,7 +50,7 @@ export class ReactPluginFactory extends PluginFactory {
 
     return new Promise((resolve, reject) => {
       if (pluginDefinition.hasComponents()) {
-        (window as any).require([scriptUrl], 
+        (window as any).require([scriptUrl],
           (components: MvdNativeReactPluginComponentDefinition) => {
             components.registerComponentFactories();
           },
@@ -79,7 +79,8 @@ export class ReactPluginFactory extends PluginFactory {
                 provide: ReactEntryHook,
                 useValue: reactHook
               }
-            ]
+            ],
+            jit: true
           })
           class ReactPluginModule {}
           resolve(this.compiler.compileModuleAsync(ReactPluginModule).then(factory =>
@@ -98,9 +99,9 @@ export class ReactPluginFactory extends PluginFactory {
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
 
