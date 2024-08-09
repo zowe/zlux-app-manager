ad3df038978d7c9dc8cf3aaecef8d8c3e06c9f19
Satish Wadkar <swadkar@rocketsoftware.com>
2024-08-09 16:04:38 +0530

    code refactoring

--- virtual-desktop/src/app/application-manager/application-manager.service.ts
+++ virtual-desktop/src/app/application-manager/application-manager.service.ts
@@ -24,8 +24,7 @@ import { PluginManager } from 'app/plugin-manager/shared/plugin-manager';
 import { LoadFailureComponent } from './load-failure/load-failure.component';
 import { InjectionManager } from './injection-manager/injection-manager.service';
 import { ApplicationInstance } from './application-instance';
-// import { FailureModule } from './load-failure/failure.module';
-// import { ViewportId } from './viewport-manager/viewport';
+import { FailureModule } from './load-failure/failure.module';
 import { ViewportManager } from './viewport-manager/viewport-manager.service';
 import { EmbeddedInstance } from 'pluginlib/inject-resources';
 import { BaseLogger } from 'virtual-desktop-logger';
@@ -34,7 +33,6 @@ import { LanguageLocaleService } from '../i18n/language-locale.service';
 
 @Injectable()
 export class ApplicationManager implements MVDHosting.ApplicationManagerInterface {
-  // private failureModuleFactory: NgModuleFactory<FailureModule>;
   private applicationInstances: Map<MVDHosting.InstanceId, ApplicationInstance>;
   private nextInstanceId: MVDHosting.InstanceId;
   private readonly logger: ZLUX.ComponentLogger = BaseLogger;
@@ -46,11 +44,10 @@ export class ApplicationManager implements MVDHosting.ApplicationManagerInterfac
     private viewportManager: ViewportManager,   // convention in angular is that injectable singleton provider from module will by keyed by type and placed in slot.
     private pluginManager: PluginManager,
     private injectionManager: InjectionManager,
-    // private compiler: Compiler,
     private languageLocaleService: LanguageLocaleService,
     private http: HttpClient,
+    private environmentInjector: EnvironmentInjector
   ) {
-    // this.failureModuleFactory = this.compiler.compileModuleSync(FailureModule);
     this.applicationInstances = new Map();
     this.knownLoggerMessageChecks = [];
     this.nextInstanceId = 0;
@@ -123,11 +120,6 @@ export class ApplicationManager implements MVDHosting.ApplicationManagerInterfac
     return applicationInstance;
   }
 
-  // private instantiateApplicationInstance(instance: ApplicationInstance, moduleFactory: NgModuleFactory<any>, injector: Injector): void {
-  //   const moduleRef = moduleFactory.create(injector);
-  //   instance.setModuleRef(moduleRef);
-  // }
-
   // This is the component instantiator and injector
 
   private generateComponentRefFor(instance: ApplicationInstance, viewportId: MVDHosting.ViewportId, component: Type<any>): void {
@@ -184,9 +176,7 @@ export class ApplicationManager implements MVDHosting.ApplicationManagerInterfac
         // so we return as to not repeat the process and create two Viewports for one instance
         return applicationInstance.instanceId;
       }
-      const injector = this.injectionManager.generateModuleInjector(plugin, launchMetadata, applicationInstance.instanceId, messages);
-    
-      // this.instantiateApplicationInstance(applicationInstance, compiled.moduleFactory, injector);
+    const injector = this.injectionManager.generateModuleInjector(plugin, launchMetadata, applicationInstance.instanceId, messages, compiled.providers);
       const moduleRef = createNgModule(compiled.ngModule, injector);
       applicationInstance.setModuleRef(moduleRef);
       this.logger.debug("ZWED5295I", plugin.getIdentifier(), compiled.initialComponent); //this.logger.debug(`appMgr spawning plugin ID=${plugin.getIdentifier()}, `
@@ -260,8 +250,8 @@ export class ApplicationManager implements MVDHosting.ApplicationManagerInterfac
         }
       })
       .catch((errors) => {
-        // const injector = this.injectionManager.generateFailurePluginInjector(errors);
-        // this.instantiateApplicationInstance(applicationInstance, this.failureModuleFactory, injector);
+          const injector = this.injectionManager.generateFailurePluginInjector(errors);
+          applicationInstance.setModuleRef(createNgModule(FailureModule, injector));
         applicationInstance.setMainComponent(LoadFailureComponent);
         this.generateMainComponentRefFor(applicationInstance, viewportId);
 
