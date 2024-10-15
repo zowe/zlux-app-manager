7e4c3882b72aeb5158da58982dd749ceeea1441e
Leanid Astrakou <LeanyIA@yahoo.com>
2024-10-15 16:05:13 -0400

    Revert "Integrate new @zlux/widgets(v18) angular library "

--- virtual-desktop/src/app/plugin-manager/plugin-factory/angular2/angular2-plugin-factory.ts
+++ virtual-desktop/src/app/plugin-manager/plugin-factory/angular2/angular2-plugin-factory.ts
@@ -145,9 +145,7 @@ export class Angular2PluginFactory extends PluginFactory {
           this.translationLoaderService.getTranslationProviders(pluginDefinition.getBasePlugin()).then(providers => {
             resolve(new CompiledPlugin(plugin.pluginComponent, plugin.pluginModule, providers));
           });
-        },
-        (failure: any) => reject(failure)
-      );
+        });
     });
   }
 }
