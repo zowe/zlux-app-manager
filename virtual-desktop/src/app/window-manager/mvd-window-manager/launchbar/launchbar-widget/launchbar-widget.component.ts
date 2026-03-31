1898c922325a1b34cbf0b56a985baea118712973
sambashta <sambashta@rocketsoftware.com>
2026-03-31 12:58:22 +0530

    Refactor LaunchbarWidgetComponent to remove AfterViewInit and encapsulate Zowe version retrieval
    Signed-off-by: sambashta <sambashta@rocketsoftware.com>

--- virtual-desktop/src/app/window-manager/mvd-window-manager/launchbar/launchbar-widget/launchbar-widget.component.ts
+++ virtual-desktop/src/app/window-manager/mvd-window-manager/launchbar/launchbar-widget/launchbar-widget.component.ts
@@ -17,8 +17,7 @@ import {
   OnInit,
   Output,
   ViewChild,
-  Input,
-  AfterViewInit
+  Input
 } from '@angular/core';
 import { interval } from 'rxjs';
 import { DesktopComponent, DesktopTheme } from "../../desktop/desktop.component";
@@ -36,7 +35,7 @@ import { L10nTranslationService } from 'angular-l10n';
   styleUrls: ['launchbar-widget.component.css', '../shared/shared.css'],
   providers: [LanguageLocaleService],
 })
-export class LaunchbarWidgetComponent implements MVDHosting.ZoweNotificationWatcher, OnInit, AfterViewInit {
+export class LaunchbarWidgetComponent implements MVDHosting.ZoweNotificationWatcher, OnInit {
   private readonly logger: ZLUX.ComponentLogger = BaseLogger;
   private readonly plugin: any = ZoweZLUX.pluginManager.getDesktopPlugin();
   public date: Date;
@@ -149,10 +148,11 @@ export class LaunchbarWidgetComponent implements MVDHosting.ZoweNotificationWatc
 
   ngOnInit(): void {
     this.date = new Date();
+    setZoweVersion();
     interval(1000).subscribe(() => this.date = new Date());
   }
 
-  ngAfterViewInit(): void {
+  private setZoweVersion(): void {
     ZoweZLUX.serverMetadata.getZoweVersion()
       .then((version: string) => {
         this.zoweVersion = version ? version : null;
