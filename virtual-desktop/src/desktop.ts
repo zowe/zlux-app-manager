1c46912ff4a0c26914ab9cc88c23fd0df8065a63
Timothy Gerstel <tim.gerstel@gmail.com>
2026-03-31 12:48:40 -0400

    add password reveal toggle to login screen and change password form
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- virtual-desktop/src/desktop.ts
+++ virtual-desktop/src/desktop.ts
@@ -144,6 +144,14 @@ function performBootstrap(): void {
     ));
 }
 
+// set baseurl for browser-level relative URL resolution (fonts, images, etc.)
+const baseUrl = document.createElement('base');
+baseUrl.setAttribute("href", ZoweZLUX?.uriBroker.desktopRootUri());
+
+if (document.head) {
+  document.head.appendChild(baseUrl);
+}
+
 const element = document.createElement('rs-com-root');
 document.body.appendChild(element);
 performBootstrap();
