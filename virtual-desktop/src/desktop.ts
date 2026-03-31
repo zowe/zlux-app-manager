cab563f008a534d0ebddca50c96060a7aa9910bb
RamkrishnaRocket1 <rdakwa@rocketsoftware.com>
2026-03-31 23:19:49 +0530

    updated the revert for this PR
    Signed-off-by: RamkrishnaRocket1 <rdakwa@rocketsoftware.com>

--- virtual-desktop/src/desktop.ts
+++ virtual-desktop/src/desktop.ts
@@ -148,7 +148,6 @@ const element = document.createElement('rs-com-root');
 document.body.appendChild(element);
 performBootstrap();
 
-
 /*
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
