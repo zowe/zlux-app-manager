0637c20ed0307e1d6bee14bf9f65b488eb256dec
RamkrishnaRocket1 <rdakwa@rocketsoftware.com>
2026-04-16 02:39:48 +0530

    desktip.ts
    Signed-off-by: RamkrishnaRocket1 <rdakwa@rocketsoftware.com>

--- virtual-desktop/src/desktop.ts
+++ virtual-desktop/src/desktop.ts
@@ -144,6 +144,14 @@ if (document.head) {
   document.head.appendChild(baseUrl);
 }
 
+// set baseurl
+const baseUrl = document.createElement('base');
+baseUrl.setAttribute("href", ZoweZLUX?.uriBroker.desktopRootUri());
+
+if (document.head) {
+  document.head.appendChild(baseUrl);
+}
+
 const element = document.createElement('rs-com-root');
 document.body.appendChild(element);
 performBootstrap();
