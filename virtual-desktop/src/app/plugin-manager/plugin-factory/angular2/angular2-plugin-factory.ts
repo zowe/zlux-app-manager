522236220bcc05a80ef0bc54af434fb7fb241aca
RamkrishnaRocket1 <rdakwa@rocketsoftware.com>
2026-04-15 15:09:40 +0530

    removingrepeast
    Signed-off-by: RamkrishnaRocket1 <rdakwa@rocketsoftware.com>

--- virtual-desktop/src/app/plugin-manager/plugin-factory/angular2/angular2-plugin-factory.ts
+++ virtual-desktop/src/app/plugin-manager/plugin-factory/angular2/angular2-plugin-factory.ts
@@ -161,8 +161,6 @@ export class Angular2PluginFactory extends PluginFactory {
  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
 
  SPDX-License-Identifier: EPL-2.0
- SPDX-License-Identifier: EPL-2.0
 
- Copyright Contributors to the Zowe Project.
  Copyright Contributors to the Zowe Project.
 */
